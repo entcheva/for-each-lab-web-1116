9bad9c34a3cbee958884087e8e37a7add3a0c44e
entcheva <christina.entcheva@gmail.com>
2017-01-17 15:51:57 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var iterativeLog = array => {
+  array.forEach((element, index) => {
+    console.log(`${index}: ${element}`);
+  });
+};
+
+
+
+
+
+
+
+var iterate = callback => {
+  arr = [1, 2, 3, 4, 5, 6];
+  arr.forEach(callback);
+  return arr;
+};
+
+var doToArray = (array, callback) => {
+  array.forEach(callback);
+};
